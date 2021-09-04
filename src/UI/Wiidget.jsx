86c10cfd495c633c508bc9574335e0c3c5df0db6
Ivan <iborisov@pryaniky.onmicrosoft.com>
2021-09-04 03:11:15 +0300

    widget

---
+++ src/UI/Wiidget.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "utils/constants";
+
+export const Widget = styled.div`
+  background: ${colors.white};
+  padding: 24px;
+  margin-top: 36;
+`
