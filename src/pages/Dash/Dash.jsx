9cc4a3a51a7682bea62b0fa88aa4be4399ead8bd
Ivan <iborisov@pryaniky.onmicrosoft.com>
2021-09-04 20:58:04 +0300

    dash page

--- src/pages/Dash/Dash.jsx
+++ src/pages/Dash/Dash.jsx
@@ -2,18 +2,28 @@ import styled from 'styled-components';
 import { mapStateToProps, mapActionsToProps } from './Dash.index';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router';
+import dash from 'assets/mock/dash.png';
+import { useEffect, useState } from 'react';
 
 const Wrapper = styled.div`
-  
+  padding: 0px;
 `
-
+const DashContent = styled.img`
+  visibility: ${props => props.visibility};
+`
 
 const Component = () => {
 
-  const { id } = useParams();
+  const [vidible, setVisible] = useState('hidden');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible('visible')
+    }, 300)
+  }, [])
 
   return <Wrapper>
-    
+    <DashContent src={dash} visibility={vidible} alt='dasth' />
   </Wrapper>
 }
 
